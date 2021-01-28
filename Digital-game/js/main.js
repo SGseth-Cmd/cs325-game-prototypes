c2ccf9fd4f5aa360ad1fc4c1122990c699a45cf5
SGseth-Cmd <78049978+SGseth-Cmd@users.noreply.github.com>
2021-01-28 17:47:43 -0500

    art fix 2

--- Digital-game/js/main.js
+++ Digital-game/js/main.js
@@ -21,7 +21,7 @@ class MyScene extends Phaser.Scene {
     
     preload() {
         // Load an image and call it 'logo'.
-        this.load.image( 'emblem', 'asset/emblem.png' );
+        this.load.image( 'emblem', 'assets/emblem.png' );
     }
     
     create() {
